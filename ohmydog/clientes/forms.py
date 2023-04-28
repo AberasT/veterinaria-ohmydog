60a48cd8e6828b8bce24191fcab7b96fe7703f41
Álvaro Aberastain Marzorati <a.aberastain.marzorati@gmail.com>
2023-04-28 20:04:56 -0300

    agregado error cuando hay campo faltante en el form

--- ohmydog/clientes/forms.py
+++ ohmydog/clientes/forms.py
@@ -2,9 +2,11 @@ from django.forms import Form, IntegerField, CharField,ModelForm
 from .models import Cliente
 from django import forms
 
+error_messages = {"required": "Se deben completar todos los campos"}
+
 class RegistrarClienteForm(Form):
-    dni = IntegerField(label="DNI", required=True)
-    nombre = CharField(label="Nombre", max_length=30, required=True)
-    apellido = CharField(label="Apellido", max_length=30, required=True)
-    email = CharField(widget=forms.EmailInput, label="Email", required=True)
-    clave = forms.CharField(widget=forms.PasswordInput, required=True)
+    dni = IntegerField(label="DNI", required=True, error_messages=error_messages)
+    nombre = CharField(label="Nombre", max_length=30, required=True, error_messages=error_messages)
+    apellido = CharField(label="Apellido", max_length=30, required=True, error_messages=error_messages)
+    email = CharField(widget=forms.EmailInput, label="Email", required=True, error_messages=error_messages)
+    clave = forms.CharField(widget=forms.PasswordInput, required=True, error_messages=error_messages)
